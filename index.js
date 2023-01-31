38c1b0ea70979fee6d266f84d7a7c7e37d26e5e7
Nolu-M <119700411+Nolu-M@users.noreply.github.com>
2023-01-31 22:00:01 +0200

    JavaScript update

---
+++ index.js
@@ -0,0 +1,31 @@
+let homeEl = document.getElementById("score-el1")
+let home = 12
+
+function home1() {
+    home += 1
+    homeEl.innerText = home
+}
+function home2() {
+    home += 2
+    homeEl.innerText = home
+}
+function home3() {
+    home += 3
+    homeEl.innerText = home
+}
+
+let guestEl = document.getElementById("score-el2")
+let guest = 5
+
+function guest1() {
+    guest += 1
+    guestEl.innerText = guest
+}
+function guest2() {
+    guest += 2
+    guestEl.innerText = guest
+}
+function guest3() {
+    guest += 3
+    guestEl.innerText = guest
+}
